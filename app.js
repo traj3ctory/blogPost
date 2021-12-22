4619153e46624979aa228441cce5e122ed845f4a
traj3ctory <davidbenard.bd@gmail.com>
2021-12-22 14:52:17 +0100

    build: add dotenv module

--- app.js
+++ app.js
@@ -1,44 +1,46 @@
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const blogRoutes = require('./routes/blogRoutes');
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const blogRoutes = require("./routes/blogRoutes");
+require('dotenv').config();
 
 // express app
 const app = express();
 
 // connect to mongodb & listen for requests
-const dbURI = `mongodb+srv://traj3ctory:applocation@cluster0.ic2kd.mongodb.net/blog?retryWrites=true&w=majority`;
+const dbURI = `mongodb+srv://${process.env.USER_ID}:${process.env.USER_KEY}@cluster0.ic2kd.mongodb.net/blog?retryWrites=true&w=majority`;
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen(3000))
-  .then(result => console.log('Listening on port 3000'))
-  .catch(err => console.log(err));
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .then((result) => console.log("Listening on port 3000"))
+  .catch((err) => console.log(err));
 
 // register view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // middleware & static files
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use((req, res, next) => {
   res.locals.path = req.path;
   next();
 });
 
 // routes
-app.get('/', (req, res) => {
-  res.redirect('/blogs');
+app.get("/", (req, res) => {
+  res.redirect("/blogs");
 });
 
-app.get('/about', (req, res) => {
-  res.render('about', { title: 'About' });
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
 });
 
 // blog routes
-app.use('/blogs', blogRoutes);
+app.use("/blogs", blogRoutes);
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).render('404', { title: '404' });
+  res.status(404).render("404", { title: "404" });
 });
